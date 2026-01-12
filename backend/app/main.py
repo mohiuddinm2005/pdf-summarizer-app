a379af7e0b3a6a059474a48c4ed401d487446430
Mustafa Mohiuddin <mustafamohiuddin12345@gmail.com>
2026-01-11 20:56:30 -0500

    made changes to virtual envrioment for activation

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 """
-The project will deploy a python script contating the Gemini API. 
+The project will deploy a python script containing the Gemini API. 
 Vercel is deployed via front end with a react base.
 Each pdf file is read in a concise and clean manner
 A fastAPI implementation will be contained and documented below 
