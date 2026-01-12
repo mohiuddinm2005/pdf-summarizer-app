5573d0b97f5a53164d5195d0e5d580fc69483ead
Mustafa Mohiuddin <mustafamohiuddin12345@gmail.com>
2026-01-12 12:42:29 -0500

    Reupdated backend to clean up files and run pdf scanner

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,37 +14,46 @@ from pypdf import PdfReader
 from pdfminer.high_level import extract_text as fallback_text_extraction
 import os 
 import base64
+import uuid
 
 app = FastAPI()
 
-pdf_path = ""
+UPLOAD_DIR = "uploads" 
 
 
 # api key 
 client = genai.Client(api_key = "AIzaSyDdHtUezHckjpOLJ4ZkA3jgcBNE3EWsSiI")
-
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 # pdf extraction and parsing into txt format
-def pdf_extraction(pdf_path = str):
-    reader = PdfReader(pdf_path)
+def pdf_extraction(path: str):
+    reader = PdfReader(path)
     text = ""
     try:
         for page in reader.pages:
             text += page.extract_text()
         return text.strip()
     except ValueError as exc:
-        text = fallback_text_extraction(pdf_path)
+        text = fallback_text_extraction(path)
     
 
 # upload file with this function 
 @app.post("/upload")
-async def pdf_reader_upload(UploadFile = File(pdf_path)):  
+async def pdf_reader_upload(UploadFile = File()):  
+    
+    # save files to path
+    file_id = str(uuid.uuid4())
+    file_path = os.path.join(UPLOAD_DIR, f"{file_id}.pdf")
+
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
 
     # extraction of the text from pdf_extraction function passing through LLM
-    pdf_source = pdf_extraction(pdf_path)
+    pdf_text = pdf_extraction(file_path)
 
-    with open(pdf_path, "rb") as f:
-        base64_pdf = base64.b64encode(f.read().decode('utf-8'))
+    # read the pdf file content 
+    if not pdf_text:
+        return {"error": "Could not extract text from PDF"}
     
 
     response = client.models.generate_content(
@@ -52,13 +61,15 @@ async def pdf_reader_upload(UploadFile = File(pdf_path)):
     You need to summarize the contents and give the user key ideas in the pdf. 
     Read the pdf and provide a brief and short explanation to the user. 
     
-    PDF content: {pdf_source[:12000]} """)
+    PDF content: {pdf_text[:12000]} """)
 
     summary = response.text
 
     # return status of summary and print result 
     return {"status": "working on it....",
             "summary": summary}
+    
+    
 
 
 @app.get("/")
