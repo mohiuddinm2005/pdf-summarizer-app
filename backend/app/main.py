890de6453af4f3cf8cf9de191748f6b5d45cee96
Mustafa Mohiuddin <mustafamohiuddin12345@gmail.com>
2026-01-12 21:40:00 -0500

    updated file to store errors for gemini api handling

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,7 +30,9 @@ def pdf_extraction(path: str):
     text = ""
     try:
         for page in reader.pages:
-            text += page.extract_text()
+            page_text = page.extract_text()
+            if page_text:
+                text += page_text
         return text.strip()
     except ValueError as exc:
         text = fallback_text_extraction(path)
@@ -59,15 +61,19 @@ async def pdf_reader_upload(file: UploadFile = File()):
     if not pdf_text:
         return {"error": "Could not extract text from PDF"}
     
+    # throws error is api call is not valid
+    try:
+        response = client.models.generate_content(
+        model="gemini-2.5-flash", contents = f""" You are a sophisticated model designed to summarize, deduce, and breakdown a pdf.
+        You need to summarize the contents and give the user key ideas in the pdf. 
+        Read the pdf and provide a brief and short explanation to the user. 
+        
+        PDF content: {pdf_text[:12000]} """)
 
-    response = client.models.generate_content(
-    model="gemini-2.5-flash", contents = f""" You are a sophisticated model designed to summarize, deduce, and breakdown a pdf.
-    You need to summarize the contents and give the user key ideas in the pdf. 
-    Read the pdf and provide a brief and short explanation to the user. 
-    
-    PDF content: {pdf_text[:12000]} """)
+        summary = response.text
+    except Exception as e:
+        raise HTTPException (status_code = 500, detail=f"fatal api crash {e}")
 
-    summary = response.text
 
     # return JSON status of summary and print result 
     return {
